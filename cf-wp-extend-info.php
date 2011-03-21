723f3178439e0e910df35fdbbdf236070655b76b
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-21 20:06:56 +0000

    function `themes_api` moved in WP 3.1
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-wp-extend-info/trunk@34927 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-wp-extend-info.php
+++ cf-wp-extend-info.php
@@ -70,7 +70,9 @@ function cfwpei_get_remote_data($slug, $type) {
 			$data = plugins_api('plugin_information', array('slug' => $slug));
 			break;
 		case 'theme':
-			require_once(ABSPATH.'wp-admin/includes/theme-install.php');
+			global $wp_version;
+			$file = 'theme'.(version_compare('3.1', $wp_version, '<') ? '-install', '').'.php';
+			require_once(ABSPATH.'wp-admin/includes/'.$file);
 			$data = themes_api('theme_information', array('slug' => $slug));
 			break;
 	}
